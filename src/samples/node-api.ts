9193cfd2faf245a035182ff0ed3873dd2e353232
Joris Mansion <juhh@juhh-core.local>
2023-12-10 22:02:34 +0100

    clean boilerplate

--- src/samples/node-api.ts
+++
@@ -1,9 +0,0 @@
-import { lstat } from 'node:fs/promises'
-import { cwd } from 'node:process'
-import { ipcRenderer } from 'electron'
-
-lstat(cwd()).then(stats => {
-  console.log('[fs.lstat]', stats)
-}).catch(err => {
-  console.error(err)
-})
